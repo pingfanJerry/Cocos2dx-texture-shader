f715aa66567f9bd4c6a41a234881045f221cad22
Tjaž Hrovat <tjaz@Tjazs-Mac.local>
2013-10-05 09:07:28 +0200

    Initial Commit

---
+++ Cocos2d-x texture/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  Cocos2d_x_textureAppController.h
+//  Cocos2d-x texture
+//
+//  Created by Tjaz Hrovat on 10/5/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ Cocos2d-x texture/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  Cocos2d_x_textureAppController.h
+//  Cocos2d-x texture
+//
+//  Created by Tjaz Hrovat on 10/5/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
